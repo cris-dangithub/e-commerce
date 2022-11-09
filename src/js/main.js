b01452174896655a80965e9aced8236960983c36
cris-dangithub <cristiandaniel8080@gmail.com>
2022-11-09 16:20:35 -0500

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+mixitup(".shop__products" , {
+    selectors: {
+        target: ".products__card"
+    },
+    animation: {
+        duration: 300
+    }
+}).filter("all")
+
+
+/* Navbar menu */
+
+
+
