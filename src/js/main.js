a9709e926d1de1edc23ad3fbf8c57fb946f00811
cris-dangithub <cristiandaniel8080@gmail.com>
2022-11-10 12:18:36 -0500

    Navbar terminado, empezando carrito de compras, parte empty

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,46 @@ mixitup(".shop__products" , {
     }
 }).filter("all")
 
+// *****************************************************************************************************
+//! FUNCIONES **********************************
+function translateContainer(condition, classEnter, currentHTML) {
+    if (condition) return currentHTML.classList.toggle(classEnter)
+}
 
-/* Navbar menu */
+function changeNavBg(){
+    container.getBoundingClientRect().top >= -40 ? 
+    navbar.classList.add("main__navbarTransparent") : navbar.classList.remove("main__navbarTransparent")
+}
 
+//! VARIABLES **********************************
+const navbar = document.querySelector(".main__navbar")
+const navbar__menu = document.querySelector(".navbar__menu")
+const navbar__shoppingCart = document.querySelector(".navbar__cart")
+const container = document.querySelector(".main__container")
 
+let menuActive = false
+let shoppingCartActive = false
+
+//* WINDOW **********************************
+window.addEventListener("scroll", (e) => {
+    changeNavBg()
+})
+
+window.addEventListener("load", (e) => {
+    changeNavBg()
+})
+
+//* NAVBAR **********************************
+navbar.addEventListener("click", (e) => {
+    if (e.target.classList.contains("bx-grid-alt")) 
+        menuActive = translateContainer(!menuActive , "navbar__menuShow", navbar__menu)
+    if (e.target.classList.contains("hideMenu")) {
+        menuActive = translateContainer(menuActive , "navbar__menuShow", navbar__menu)
+        shoppingCartActive = translateContainer(shoppingCartActive , "navbar__cartShow", navbar__shoppingCart)
+    }
+
+    if (e.target.classList.contains("showCart") || e.target.classList.contains("shoppingBagIcon__count"))
+        shoppingCartActive = translateContainer(!shoppingCartActive , "navbar__cartShow", navbar__shoppingCart)
+
+})
 
